be22bcfede2a1d64580d59d894f1f5ade5b9de5c
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-07-08 10:53:15 +0200

    ADD: Hash passwords algorithm

---
+++ test.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:8080/api/register \
+  -H "Content-Type: application/json" \
+  -d '{"email":"gustavo@example.com", "username":"gusty", "password":"123456", "phone_number":"1234567890"}'

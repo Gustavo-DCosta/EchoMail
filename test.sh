772d176a58e67dce6045b4cc7974bd324982901f
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-07-08 10:55:34 +0200

    Remove tracked files that should be ignored

--- test.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://localhost:8080/api/register \
-  -H "Content-Type: application/json" \
-  -d '{"email":"gustavo@example.com", "username":"gusty", "password":"123456", "phone_number":"1234567890"}'

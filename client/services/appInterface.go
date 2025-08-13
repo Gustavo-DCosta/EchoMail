99ab22cca9678cc98ce747b97665dd0fb49a7a28
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-08-13 11:15:03 +0200

    have to push everything to fix some auth bugs

--- client/services/appInterface.go
+++ client/services/appInterface.go
@@ -8,18 +8,26 @@ import (
 	"github.com/fatih/color"
 )
 
+func HelpCommand() {
+	fmt.Print("Enter ")
+	color.RGB(224, 166, 31).Print("command")
+	fmt.Print(" (")
+	color.RGB(96, 168, 74).Print("'help' ")
+	color.RGB(92, 132, 232).Print(" for ")
+	fmt.Print(" options): \n")
+}
+
 func AppUnlocked(EmailAddress string) {
-	CenterElement("Welcome"+EmailAddress, true)
+	CenterElement("Welcome "+EmailAddress+"\n", true)
+	HelpCommand()
+	IOParser()
 }
 
 func IOParser() {
-	stdOutAppUnlocked := color.RGB(221, 211, 115)
-
-	stdOutAppUnlocked.Print("=> !")
-
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
+		fmt.Print("=> !")
 		scanner.Scan()
 		err := scanner.Err()
 
@@ -27,8 +35,20 @@ func IOParser() {
 			fmt.Println("Error starting parser: ", err)
 		}
 
-		input := scanner.Text()
+		cmdInput := scanner.Text()
 
-		fmt.Println(input)
+		switch cmdInput {
+		case "send @user":
+			MsgWS()
+		case "help":
+			StdOutInterHelp()
+		case "exit":
+			os.Exit(1)
+		case "clear":
+			ClearUI()
+		default:
+			fmt.Println("😺 Ayo captain Whiskers McGraw doesn't know that command!!")
+		}
+
 	}
 }
